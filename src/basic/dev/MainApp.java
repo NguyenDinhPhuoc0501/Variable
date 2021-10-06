f2b335b8966cc77bb9a4ebe142431cc9916fceac
NGUYỄN ĐÌNH PHƯỚC <nguyendinhphuoc05012003@gmail.com>
2021-10-06 16:35:57 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,35 @@
+package basic.dev;
+/**
+ * Ghi chú (comment nhiều dòng)
+ * @author FPTSHOP
+ *1. Tên lớp trùng tên file
+ *2. Tên lớp cái đầu viết hoa. Nếu Từ ghép thì những chữ cái đầu viết hoa 
+ *4. Tên lớp đặt phải có ý nghĩa . Ví dụ: class Peson : lớp mô tả con người
+ *- Interface: Phải có tiền tố I. Ví dự: IListener
+ *- Abstract: Phải có tiền tố Abstract. Ví dụ: AbstractShape
+ */
+
+public class MainApp {
+	//Comment 1 đong cách 1. Phím nóng Ctr + /
+	/*Comment 1 dòng cách 2*/
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+		/**
+		 * Cách đặt tên biếm:
+		 * 1. Không chứa ký tự đạcw biệt !@#$%^&*()-+=
+		 * 2. Khong chữa khoảng trắng 
+		 * 3. Viết thường
+		 * 4. Không được bắt đầu bằng số
+		 */
+		int number;
+		String nameShool;
+			
+	}
+	/*
+	 * tên phương thức tuân theo quy tắc camelCase
+	 */
+	public void show() {
+		
+	}
+
+}
